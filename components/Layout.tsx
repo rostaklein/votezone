1c9b4b28814589939dc79a4b4fc2af508d566020
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-12-11 17:09:57 +0100

    added header and footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,19 +5,49 @@ const Wrapper = styled.div`
   padding: 0 24px;
 `
 
+const TopHeaderWrapper = styled.div`
+  background-color: #1e1e1e;
+  padding: 4px 0;
+  color: #787878;
+  font-size: 13px;
+`
+
 const ContentWrapper = styled.div`
   max-width: 960px;
   margin: 0 auto;
 `
 
+const Footer = styled.footer`
+  padding: 0 24px;
+  margin: 24px auto;
+  text-align: center;
+  color: #787878;
+  font-size: 13px;
+  > span {
+    color: #adadad;
+    margin: 0 6px;
+  }
+`
+
 const Layout: React.FC = props => {
   return (
-    <Wrapper>
-      <ContentWrapper>
-        <Header />
-      </ContentWrapper>
-      <ContentWrapper>{props.children}</ContentWrapper>
-    </Wrapper>
+    <>
+      <TopHeaderWrapper>
+        <ContentWrapper>
+          The honest vote portal without thousands flashing GIFs
+        </ContentWrapper>
+      </TopHeaderWrapper>
+      <Wrapper>
+        <ContentWrapper>
+          <Header />
+        </ContentWrapper>
+        <ContentWrapper>{props.children}</ContentWrapper>
+      </Wrapper>
+      <Footer>
+        Made with love in 🇨🇿 <span>|</span> © VOTEZONE{" "}
+        {new Date().getFullYear()}
+      </Footer>
+    </>
   )
 }
 
--- components/globalStyles.ts
+++ components/globalStyles.ts
@@ -6,17 +6,19 @@ export const GlobalStyle = createGlobalStyle`
  html {
     margin: 0;
     padding: 0;
-    height: 100%;
   }
   body {
     margin: 0 auto;
     padding: 0;
-    height: 100%;
     line-height: 1.47059;
     font-weight: 400;
     font-family: "SF Pro Text", "SF Pro Icons", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
     background: url('/votezone_bg_${rand}.jpeg') top center no-repeat  #ededed;
     font-style: normal;
     padding-bottom: ${Math.random()}px;
+  }
+
+  html, body, #__next{
+    height: 100%;
   }
 `
